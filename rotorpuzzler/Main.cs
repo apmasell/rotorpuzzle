bd5787515e522b09f3024b04a29984b1943b423c
Andre Masella <andre@masella.no-ip.org>
2010-12-11 18:21:43 -0500

    Added command line interface

---
+++ rotorpuzzler/Main.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+namespace name.masella.rotorpuzzle {
+	class MainClass {
+		public static void Main(string[] args) {
+			if(args.Length != 2) {
+				Console.WriteLine("Rotor Puzzler usage: rotorpuzzler numsamples numrotorslots");
+			} else {
+				var samples = int.Parse(args[0]);
+				var rotorslots = int.Parse(args[1]);
+
+				foreach(var partitions in RotateablePartitions.Of(samples, rotorslots)) {
+					foreach(var rotor in Configurations.Of(partitions, rotorslots)) {
+						if(rotor != null) {
+							Console.Write('|');
+
+							foreach(var v in rotor) {
+								Console.Write(v ? 'X' : ' ');
+							}
+
+							Console.WriteLine('|');
+						}
+					}
+				}
+			}
+		}
+	}
+}
